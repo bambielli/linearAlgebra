9ba47cd4f2cef7770d58d609d3b0038182be17a3
bambielli <bambielli@expedia.com>
2017-05-28 15:25:32 -0500

    whoa decimal precision sucks

--- main.py
+++ main.py
@@ -4,29 +4,68 @@ from vector import Vector
 QUIZ_VECTOR_1 = Vector([8.218, -9.341])
 QUIZ_VECTOR_2 = Vector([-1.129, 2.111])
 
-print(QUIZ_VECTOR_1 + QUIZ_VECTOR_2)
+# print(QUIZ_VECTOR_1 + QUIZ_VECTOR_2)
 
 QUIZ_VECTOR_3 = Vector([7.119, 8.215])
 QUIZ_VECTOR_4 = Vector([-8.223, 0.878])
 
-print(QUIZ_VECTOR_3 - QUIZ_VECTOR_4)
+# print(QUIZ_VECTOR_3 - QUIZ_VECTOR_4)
 
 QUIZ_VECTOR_5 = Vector([1.671, -1.012, -0.318])
 SCALAR = 7.41
 
-print(QUIZ_VECTOR_5 * SCALAR)
+# print(QUIZ_VECTOR_5 * SCALAR)
 
 TEST_VECTOR = Vector([3, 4])
-print(TEST_VECTOR.magnitude())
+# print(TEST_VECTOR.magnitude())
 
 MAG_QUIZ_1 = Vector([-0.221, 7.437])
 MAG_QUIZ_2 = Vector([8.813, -1.331, -6.247])
 
-print(MAG_QUIZ_1.magnitude())
-print(MAG_QUIZ_2.magnitude())
+# print(MAG_QUIZ_1.magnitude())
+# print(MAG_QUIZ_2.magnitude())
 
 MAG_QUIZ_3 = Vector([5.581, -2.136])
 MAG_QUIZ_4 = Vector([1.996, 3.108, -4.554])
 
-print(MAG_QUIZ_3.normalize())
-print(MAG_QUIZ_4.normalize())
+# print(MAG_QUIZ_3.normalize())
+# print(MAG_QUIZ_4.normalize())
+
+
+DOT_QUIZ_1A = Vector([7.887, 4.138])
+DOT_QUIZ_1B = Vector([-8.802, 6.776])
+print(DOT_QUIZ_1A.dot(DOT_QUIZ_1B))
+
+DOT_QUIZ_2A = Vector([-5.955, -4.904, -1.874])
+DOT_QUIZ_2B = Vector([-4.496, -8.755, 7.103])
+print(DOT_QUIZ_2A.dot(DOT_QUIZ_2B))
+
+DOT_QUIZ_3A = Vector([3.183, -7.627])
+DOT_QUIZ_3B = Vector([-2.668, 5.319])
+print(DOT_QUIZ_3A.angle(DOT_QUIZ_3B))
+
+DOT_QUIZ_4A = Vector([7.35, 0.221, 5.188])
+DOT_QUIZ_4B = Vector([2.751, 8.259, 3.985])
+print(DOT_QUIZ_4A.angle(DOT_QUIZ_4B, True))
+
+
+
+PARALLEL_ORTH_1A = Vector([-7.579, -7.88])
+PARALLEL_ORTH_1B = Vector([22.737, 23.64])
+print("parallel: " + str(PARALLEL_ORTH_1A.is_parallel_to(PARALLEL_ORTH_1B)))
+print("orth: " + str(PARALLEL_ORTH_1A.is_orthogonal_to(PARALLEL_ORTH_1B)))
+
+PARALLEL_ORTH_2A = Vector([-2.029, 9.97, 4.172])
+PARALLEL_ORTH_2B = Vector([-9.231, -6.639, -7.245])
+print("parallel: " + str(PARALLEL_ORTH_2A.is_parallel_to(PARALLEL_ORTH_2B)))
+print("orth: " + str(PARALLEL_ORTH_2A.is_orthogonal_to(PARALLEL_ORTH_2B)))
+
+PARALLEL_ORTH_3A = Vector([-2.328, -7.284, -1.214])
+PARALLEL_ORTH_3B = Vector([-1.821, 1.072, -2.94])
+print("parallel: " + str(PARALLEL_ORTH_3A.is_parallel_to(PARALLEL_ORTH_3B)))
+print("orth: " + str(PARALLEL_ORTH_3A.is_orthogonal_to(PARALLEL_ORTH_3B)))
+
+PARALLEL_ORTH_4A = Vector([2.118, 4.827])
+PARALLEL_ORTH_4B = Vector([0, 0])
+print("parallel: " + str(PARALLEL_ORTH_4A.is_parallel_to(PARALLEL_ORTH_4B)))
+print("orth: " + str(PARALLEL_ORTH_4A.is_orthogonal_to(PARALLEL_ORTH_4B)))
--- vector.py
+++ vector.py
@@ -1,17 +1,22 @@
 """ Vector - an object that defines a vector """
-from math import sqrt
+from math import sqrt, acos, degrees, pi
+from decimal import Decimal, getcontext
+
+getcontext().prec = 4
+
 class Vector(object):
     """ A Vector object
 
         constructor:
         coordinates -- an iterable
     """
+    cannot_normalize_zero_vector = "Cannot normalize the zero vector"
 
     def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = tuple(coordinates)
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
             self.dimension = len(coordinates)
 
         except ValueError:
@@ -28,45 +33,97 @@ class Vector(object):
 
     def __add__(self, vector):
         """ adds vector to the current vector
-            mutates the current vector
+            returns a new vector
         """
         return tuple([
             x + y
-            for (x, y) in zip(self.coordinates, vector.coordinates)
-        ])
+            for (x, y) in zip(self.coordinates, vector.coordinates)])
 
 
     def __sub__(self, vector):
         """ subtracts vector from the current vector
-            mutates the current vector
+            returns a new vector
         """
         return tuple([
             x - y
-            for (x, y) in zip(self.coordinates, vector.coordinates)
-        ])
+            for (x, y) in zip(self.coordinates, vector.coordinates)])
 
-    def __mul__(self, other):
+    def __mul__(self, scalar):
         """ multiplies vector by scaling factor
-            mutates the current vector
+            returns a new vector
         """
-        if isinstance(other, Vector):
-            return tuple([
-                x * y
-                for (x, y) in zip(self.coordinates, other.coordinates)
-            ])
-        else:
-            return tuple([
-                other * x
-                for x in self.coordinates
-            ])
+        return tuple([
+            Decimal(scalar)*x
+            for x in self.coordinates])
 
     def magnitude(self):
-        """ returns the magnitude of the current vector """
-        return sqrt(sum(x * x for x in self.coordinates))
+        """ returns the magnitude of the current vector
+
+            magnitude is a value that indicates how much distance the vector covers
+        """
+        return Decimal(sqrt(sum(x**2 for x in self.coordinates)))
 
     def normalize(self):
-        """ returns normalized vector for the current vector """
+        """ returns normalized vector for the current vector
+
+            normalized vector is the unit vector that is in the same direction
+            as the given vector. Unit vector is vector of magnitude 1 in a given direction
+        """
         try:
-            return Vector(self * (1./self.magnitude()))
+            magnitude = self.magnitude()
+            return Vector(self * (Decimal('1.0')/magnitude))
         except ZeroDivisionError:
-            raise Exception('Cannot normalize the zero vector')
+            raise ZeroDivisionError(self.cannot_normalize_zero_vector)
+
+    def dot(self, vector):
+        """ returns the dot product of the two vectors
+
+            dot product allows us to find the angle between two vectors
+        """
+        return sum(x * y for x, y in zip(self.coordinates, vector.coordinates))
+
+    def angle(self, vector, in_degrees=False):
+        """ returns the angle between the two vectors """
+        # rad_val = acos(self.dot(vector) / (self.magnitude() * vector.magnitude()))
+        try:
+            norm_1 = self.normalize()
+            norm_2 = vector.normalize()
+            rad_val = acos(norm_1.dot(norm_2))
+            print(rad_val)
+            if in_degrees:
+                return degrees(rad_val)
+            else:
+                return rad_val
+        except ZeroDivisionError:
+            raise Exception("Cannot compute angle with the zero vector")
+
+    def is_parallel_to(self, vector):
+        """ returns a boolean indicating if the two vectors are parallel
+            true when the vectors are scalar multiples of each other
+        """
+        # try:
+        #     scalar_list = [
+        #         x / y
+        #         for x, y in zip(self.coordinates, vector.coordinates)
+        #     ]
+        #     return len(set(scalar_list)) == 1
+        # except ZeroDivisionError:
+        #     return True
+        return (self.is_zero() or
+                vector.is_zero() or
+                self.angle(vector) == 0 or
+                self.angle(vector) == pi)
+
+    def is_orthogonal_to(self, vector, tolerance=1e-10):
+        """ returns a boolean indicating if the two vectors are orthogonal
+            true when dot product between two vectors is 0
+            (which indicates that the cos of the angle between them is 0)
+        """
+        return abs(self.dot(vector)) < tolerance
+
+    def is_zero(self, tolerance=1e-10):
+        """ returns a boolean indicating whether the vector is zero (to a tolerance)
+            We know a vector is zero when its magnitude is zero
+        """
+
+        return self.magnitude() < tolerance
